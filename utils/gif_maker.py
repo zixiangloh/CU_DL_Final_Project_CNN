e39870110e4e3fe337e03a1d29fd31f6b4fc0bf4
zixiangloh <zixiangloh@gmail.com>
2022-04-21 19:04:06 -0700

    Updating gif maker

--- utils/gif_maker.py
+++ utils/gif_maker.py
@@ -5,16 +5,17 @@ from PIL import Image
 
 
 def make_gif(frame_folder, out_file):
-    frames = [Image.open(image) for image in glob.glob(os.path.join(frame_folder, "*.png"))]
-    random.shuffle(frames)
+    file_lists = glob.glob(os.path.join(frame_folder, "*.png"))
+    random.shuffle(file_lists)
+    file_lists_subset = file_lists[:20]
+    frames = [Image.open(image) for image in file_lists_subset]
     # Pick a subset of frames
-    frames_subset = frames[:20]
-    frame_one = frames_subset[0]
+    frame_one = frames[0]
     frame_one.save(out_file, format="GIF", append_images=frames,
                    save_all=True, duration=1000, loop=0)
 
 
 if __name__ == "__main__":
-    input_frame_path = os.path.join("..", "data", "biased_cars_blurred", "GREEN_CARS", "train", "images")
-    output_file = os.path.join("..", "docs", "images", "biased_cars_blurred_samples.gif")
+    input_frame_path = os.path.join("..", "data", "mnist_clothing", "self_generated", "eight_by_nine_seen_images")
+    output_file = os.path.join("..", "docs", "images", "mnist_clothing_samples.gif")
     make_gif(input_frame_path, output_file)
